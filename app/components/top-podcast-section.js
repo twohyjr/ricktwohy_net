ff39093320477d04e6bc8bc76dc1eb5e6b8e67bf
Rick Twohy <twohyjr@gmail.com>
2017-11-26 21:59:03 -0600

    Added Podcast section

---
+++ app/components/top-podcast-section.js
@@ -0,0 +1,38 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+
+    favoritePodcasts:[
+        {
+            title: "Programming Throwdown",
+            logoUrl: "https://firebasestorage.googleapis.com/v0/b/personal-website-2ae94.appspot.com/o/podcast-images%2FprogrammingThrowdownLogo.png?alt=media&token=8ab14dc1-1216-47da-8d0d-c2975c9258d6",
+            locationUrl: "http://www.programmingthrowdown.com/"
+        },
+        {
+            title: "Stuff You Should Know",
+            logoUrl: "https://firebasestorage.googleapis.com/v0/b/personal-website-2ae94.appspot.com/o/podcast-images%2FstuffYouShouldKnowLogo.png?alt=media&token=c4e2dee3-6b7d-4c35-be3f-dfdc5d0124de",
+            locationUrl: "https://www.stuffyoushouldknow.com/"
+        },
+        {
+            title: "Radio Lab",
+            logoUrl: "https://firebasestorage.googleapis.com/v0/b/personal-website-2ae94.appspot.com/o/podcast-images%2FradioLabLogo.png?alt=media&token=2a9cb156-e6a5-4feb-ba72-f9178a09afbf",
+            locationUrl: "http://www.radiolab.org/"
+        },
+        {
+            title: "Coffee Break French",
+            logoUrl: "https://firebasestorage.googleapis.com/v0/b/personal-website-2ae94.appspot.com/o/podcast-images%2FcoffeeBreakFrenchLogo.png?alt=media&token=6ab73d82-872c-4667-8311-0ba131317c10",
+            locationUrl: "https://radiolingua.com/coffeebreakfrench/"
+        },
+        {
+            title: "Coffee Break Spanish",
+            logoUrl: "https://firebasestorage.googleapis.com/v0/b/personal-website-2ae94.appspot.com/o/podcast-images%2FcoffeeBreakSpanishLogo.png?alt=media&token=afa17ec2-9afe-4441-a475-40ba53f03da5",
+            locationUrl: "https://radiolingua.com/coffeebreakspanish/"
+        },
+        {
+            title: "Drunks & Dragons",
+            logoUrl: "https://firebasestorage.googleapis.com/v0/b/personal-website-2ae94.appspot.com/o/podcast-images%2FdrunksAndDragonsLogo.png?alt=media&token=0da8f6d8-8b28-489c-a79c-0884c4daba5b",
+            locationUrl: "http://geeklyinc.com/category/drunks-and-dragons/"
+        }
+    ]
+
+});
