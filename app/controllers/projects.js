c53fb3728cd8b8a33dcfe30e6c913db0baf4959b
Rick Twohy <twohyjr@gmail.com>
2017-11-26 22:31:48 -0600

    Revert "Added Projects"
    This reverts commit c6301d59bc0c92c6f61c2435ad4acbac300143c7.

--- app/controllers/projects.js
+++
@@ -1,6 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Controller.extend({
-    sortedProjects: Ember.computed.sort('model', 'sortDefinition'),
-    sortDefinition: ['sortOrder:asc'],
-});
